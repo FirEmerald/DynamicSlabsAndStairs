41baca3fc925121aaf134b096b59c7fa323d7425
FirEmerald <mccaslinwalt@gmail.com>
2024-10-25 18:35:56 -0500

    Fix server-side issues

--- src/main/java/com/firemerald/additionalplacements/network/client/ClientPlayPacket.java
+++ src/main/java/com/firemerald/additionalplacements/network/client/ClientPlayPacket.java
@@ -4,13 +4,24 @@ import com.firemerald.additionalplacements.network.APNetwork;
 import com.firemerald.additionalplacements.network.APPacket;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkEvent;
 
 public abstract class ClientPlayPacket extends APPacket {
 	@Override
 	public NetworkDirection getDirection() {
 		return NetworkDirection.PLAY_TO_CLIENT;
 	}
+	
+    @Override
+	public void handle(NetworkEvent.Context context) {
+    	handleClient(context);
+    }
+	
+	@OnlyIn(Dist.CLIENT)
+	protected abstract void handleClient(NetworkEvent.Context context);
 	
     public void sendToClient(ServerPlayer player) {
     	APNetwork.sendToClient(this, player);
--- src/main/java/com/firemerald/additionalplacements/network/server/ServerPlayPacket.java
+++ src/main/java/com/firemerald/additionalplacements/network/server/ServerPlayPacket.java
@@ -4,12 +4,20 @@ import com.firemerald.additionalplacements.network.APNetwork;
 import com.firemerald.additionalplacements.network.APPacket;
 
 import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkEvent;
 
 public abstract class ServerPlayPacket extends APPacket {
 	@Override
 	public NetworkDirection getDirection() {
 		return NetworkDirection.PLAY_TO_SERVER;
 	}
+	
+    @Override
+	public void handle(NetworkEvent.Context context) {
+    	handleServer(context);
+    }
+	
+	protected abstract void handleServer(NetworkEvent.Context context);
 	
     public void sendToServer()
     {
