58cf0830c8cff31bf72d52ced5068ba9b1a31f89
FirEmerald <mccaslinwalt@gmail.com>
2024-10-29 18:09:39 -0500

    Rename "bootup" to "startup"
    matches up to NeoForge 1.21 startup config type

--- src/main/java/com/firemerald/additionalplacements/config/APConfigs.java
+++ src/main/java/com/firemerald/additionalplacements/config/APConfigs.java
@@ -15,42 +15,42 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class APConfigs {
-    private static ConfigBootup bootup;
-    private static ForgeConfigSpec bootupSpec;
-    private static ConfigCommon common;
+    private static StartupConfig startup;
+    private static ForgeConfigSpec startupSpec;
+    private static CommonConfig common;
     private static ForgeConfigSpec commonSpec;
-    private static ConfigServer server;
+    private static ServerConfig server;
     private static ForgeConfigSpec serverSpec;
-    private static ConfigClient client;
+    private static ClientConfig client;
     private static ForgeConfigSpec clientSpec;
     
     public static void init() {
     	ModConfigEvents.loading(AdditionalPlacementsMod.MOD_ID).register(APConfigs::onModConfigLoaded);
     	ModConfigEvents.reloading(AdditionalPlacementsMod.MOD_ID).register(APConfigs::onModConfigReloaded);
-        final Pair<ConfigBootup, ForgeConfigSpec> bootupSpecPair = new ForgeConfigSpec.Builder().configure(ConfigBootup::new);
-        bootup = bootupSpecPair.getLeft();
-        bootupSpec = bootupSpecPair.getRight();
-		bootup.loadConfig(FabricLoader.getInstance().getConfigDir().resolve("additionalplacements-bootup.toml"), bootupSpec);
-        final Pair<ConfigCommon, ForgeConfigSpec> commonSpecPair = new ForgeConfigSpec.Builder().configure(ConfigCommon::new);
+        final Pair<StartupConfig, ForgeConfigSpec> startupSpecPair = new ForgeConfigSpec.Builder().configure(StartupConfig::new);
+        startup = startupSpecPair.getLeft();
+        startupSpec = startupSpecPair.getRight();
+		startup.loadConfig(FabricLoader.getInstance().getConfigDir().resolve("additionalplacements-startup.toml"), startupSpec);
+        final Pair<CommonConfig, ForgeConfigSpec> commonSpecPair = new ForgeConfigSpec.Builder().configure(CommonConfig::new);
         common = commonSpecPair.getLeft();
         ModLoadingContext.registerConfig(AdditionalPlacementsMod.MOD_ID, ModConfig.Type.COMMON, commonSpec = commonSpecPair.getRight());
-        final Pair<ConfigServer, ForgeConfigSpec> serverSpecPair = new ForgeConfigSpec.Builder().configure(ConfigServer::new);
+        final Pair<ServerConfig, ForgeConfigSpec> serverSpecPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
         server = serverSpecPair.getLeft();
         ModLoadingContext.registerConfig(AdditionalPlacementsMod.MOD_ID, ModConfig.Type.SERVER, serverSpec = serverSpecPair.getRight());
-        final Pair<ConfigClient, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(ConfigClient::new);
+        final Pair<ClientConfig, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
         client = clientSpecPair.getLeft();
         ModLoadingContext.registerConfig(AdditionalPlacementsMod.MOD_ID, ModConfig.Type.CLIENT, clientSpec = clientSpecPair.getRight());
     }
     
-    public static ConfigBootup bootup() {
-    	return bootup;
+    public static StartupConfig bootup() {
+    	return startup;
     }
     
-    public static ConfigCommon common() {
+    public static CommonConfig common() {
     	return common;
     }
     
-    public static ConfigServer server() {
+    public static ServerConfig server() {
     	return server;
     }
     
@@ -58,7 +58,7 @@ public class APConfigs {
     	return serverSpec.isLoaded();
     }
     
-    public static ConfigClient client() {
+    public static ClientConfig client() {
     	return client;
     }
     
