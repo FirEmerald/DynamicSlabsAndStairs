ad4946cda28ec0fd6dfb37f4ecce17ce3d7cd2ee
FirEmerald <mccaslinwalt@gmail.com>
2024-10-29 18:09:24 -0500

    Rename "bootup" to "startup"
    matches up to NeoForge 1.21 startup config type

--- src/main/java/com/firemerald/additionalplacements/common/CommonModEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/common/CommonModEventHandler.java
@@ -44,7 +44,7 @@ public class CommonModEventHandler
 		if (!lockGenerationTypeRegistry) {
 			lockGenerationTypeRegistry = true;
 			APConfigs.init();
-			APConfigs.BOOTUP.loadConfig(FMLPaths.CONFIGDIR.get().resolve("additionalplacements-bootup.toml"), APConfigs.BOOTUP_SPEC);
+			APConfigs.STARTUP.loadConfig(FMLPaths.CONFIGDIR.get().resolve("additionalplacements-startup.toml"), APConfigs.STARTUP_SPEC);
 		}
 	}
 	
