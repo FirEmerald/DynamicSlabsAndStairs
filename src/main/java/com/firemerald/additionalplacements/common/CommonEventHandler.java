40f2ed87c35bfa953192bee58d0d9b50f77d0bfd
FirEmerald <mccaslinwalt@gmail.com>
2024-10-28 22:24:50 -0500

    Fix some stair shapes
    fix up-facing "twist" stairs having inverted physics
    fix some vertical-facing outer stairs having the wrong model
    add a command to generate a "stair shape debugger" - a structure of sorts that contains every single possible stair connection state
    restore accidentally removed shape flipped field

--- src/main/java/com/firemerald/additionalplacements/common/CommonEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/common/CommonEventHandler.java
@@ -3,6 +3,7 @@ package com.firemerald.additionalplacements.common;
 import com.firemerald.additionalplacements.AdditionalPlacementsMod;
 import com.firemerald.additionalplacements.block.interfaces.IPlacementBlock;
 import com.firemerald.additionalplacements.commands.CommandExportTags;
+import com.firemerald.additionalplacements.commands.CommandGenerateStairsDebugger;
 import com.firemerald.additionalplacements.config.APConfigs;
 import com.firemerald.additionalplacements.network.CheckDataConfigurationTask;
 
@@ -44,6 +45,7 @@ public class CommonEventHandler
 	public static void onRegisterCommands(RegisterCommandsEvent event)
 	{
 		CommandExportTags.register(event.getDispatcher());
+		CommandGenerateStairsDebugger.register(event.getDispatcher(), event.getBuildContext());
 	}
 
 	@SubscribeEvent
