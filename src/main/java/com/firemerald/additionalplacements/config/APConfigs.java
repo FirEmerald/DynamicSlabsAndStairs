8878a788b996c27be247f535fe39887057e91728
FirEmerald <mccaslinwalt@gmail.com>
2024-10-29 18:33:02 -0500

    Make configs safer
    Configs are not built in a static initializer anymore

--- src/main/java/com/firemerald/additionalplacements/config/APConfigs.java
+++ src/main/java/com/firemerald/additionalplacements/config/APConfigs.java
@@ -11,44 +11,63 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 public class APConfigs {
-    public static final StartupConfig STARTUP;
-    public static final ForgeConfigSpec STARTUP_SPEC;
-    public static final CommonConfig COMMON;
-    public static final ForgeConfigSpec COMMON_SPEC;
-    public static final ServerConfig SERVER;
-    public static final ForgeConfigSpec SERVER_SPEC;
-    public static final ClientConfig CLIENT;
-    public static final ForgeConfigSpec CLIENT_SPEC;
+    private static StartupConfig startup;
+    private static CommonConfig common;
+    private static ForgeConfigSpec commonSpec;
+    private static ServerConfig server;
+    private static ForgeConfigSpec serverSpec;
+    private static ClientConfig client;
+    private static ForgeConfigSpec clientSpec;
     
-    static {
+    public static void init() {
         final Pair<StartupConfig, ForgeConfigSpec> startupSpecPair = new ForgeConfigSpec.Builder().configure(StartupConfig::new);
-        STARTUP = startupSpecPair.getLeft();
-        STARTUP_SPEC = startupSpecPair.getRight();
+        startup = startupSpecPair.getLeft();
         final Pair<CommonConfig, ForgeConfigSpec> commonSpecPair = new ForgeConfigSpec.Builder().configure(CommonConfig::new);
-        COMMON = commonSpecPair.getLeft();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_SPEC = commonSpecPair.getRight());
+        common = commonSpecPair.getLeft();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, commonSpec = commonSpecPair.getRight());
         final Pair<ServerConfig, ForgeConfigSpec> serverSpecPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
-        SERVER = serverSpecPair.getLeft();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_SPEC = serverSpecPair.getRight());
+        server = serverSpecPair.getLeft();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, serverSpec = serverSpecPair.getRight());
         final Pair<ClientConfig, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
-        CLIENT = clientSpecPair.getLeft();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CLIENT_SPEC = clientSpecPair.getRight());
+        client = clientSpecPair.getLeft();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, clientSpec = clientSpecPair.getRight());
+
+		startup.loadConfig(FMLPaths.CONFIGDIR.get().resolve("additionalplacements-startup.toml"), startupSpecPair.getRight());
     }
     
-    public static void init() {}
+    public static StartupConfig bootup() {
+    	return startup;
+    }
+    
+    public static CommonConfig common() {
+    	return common;
+    }
+    
+    public static ServerConfig server() {
+    	return server;
+    }
+    
+    public static boolean serverLoaded() {
+    	return serverSpec.isLoaded();
+    }
+    
+    public static ClientConfig client() {
+    	return client;
+    }
     
     public static void onModConfigsLoaded(ModConfigEvent.Loading event) {
-    	if (event.getConfig().getSpec() == COMMON_SPEC) sendConfigEvent(GenerationType::onCommonConfigLoaded);
-    	else if (event.getConfig().getSpec() == SERVER_SPEC) sendConfigEvent(GenerationType::onServerConfigLoaded);
-    	else if (event.getConfig().getSpec() == CLIENT_SPEC) sendConfigEvent(GenerationType::onClientConfigLoaded);
+    	if (event.getConfig().getSpec() == commonSpec) sendConfigEvent(GenerationType::onCommonConfigLoaded);
+    	else if (event.getConfig().getSpec() == serverSpec) sendConfigEvent(GenerationType::onServerConfigLoaded);
+    	else if (event.getConfig().getSpec() == clientSpec) sendConfigEvent(GenerationType::onClientConfigLoaded);
     }
     
     public static void onModConfigsReloaded(ModConfigEvent.Reloading event) {
-    	if (event.getConfig().getSpec() == COMMON_SPEC) sendConfigEvent(GenerationType::onCommonConfigReloaded);
-    	else if (event.getConfig().getSpec() == SERVER_SPEC) sendConfigEvent(GenerationType::onServerConfigReloaded);
-    	else if (event.getConfig().getSpec() == CLIENT_SPEC) sendConfigEvent(GenerationType::onClientConfigReloaded);
+    	if (event.getConfig().getSpec() == commonSpec) sendConfigEvent(GenerationType::onCommonConfigReloaded);
+    	else if (event.getConfig().getSpec() == serverSpec) sendConfigEvent(GenerationType::onServerConfigReloaded);
+    	else if (event.getConfig().getSpec() == clientSpec) sendConfigEvent(GenerationType::onClientConfigReloaded);
     }
     
     public static void sendConfigEvent(Consumer<GenerationType<?, ?>> action) {
