985c079015f24f8cc883ff2e44dd31c254e49e71
FirEmerald <mccaslinwalt@gmail.com>
2024-10-29 19:47:38 -0500

    Make Generation Types Safer
    made fields in APGenerationTypes private and added accessors

--- src/main/java/com/firemerald/additionalplacements/block/interfaces/ICarpetBlock.java
+++ src/main/java/com/firemerald/additionalplacements/block/interfaces/ICarpetBlock.java
@@ -32,6 +32,6 @@ public interface ICarpetBlock<T extends Block> extends IFloorBlock<T>
 
 	@Override
 	public default GenerationType<?, ?> getGenerationType() {
-		return APGenerationTypes.CARPET;
+		return APGenerationTypes.carpet();
 	}
 }
