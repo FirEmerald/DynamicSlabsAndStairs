d198edf8991f5ee0c8c68b74815bdb68bc62cde7
FirEmerald <mccaslinwalt@gmail.com>
2024-10-30 18:11:15 -0500

    Fix crash when block registry is re-ordered

---
+++ src/main/java/com/firemerald/additionalplacements/mixin/MixinForgeRegistry.java
@@ -0,0 +1,30 @@
+package com.firemerald.additionalplacements.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.firemerald.additionalplacements.AdditionalPlacementsMod;
+import com.firemerald.additionalplacements.generation.Registration;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.ForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+@Mixin(value = ForgeRegistry.class, remap = false)
+public class MixinForgeRegistry
+{
+	@SuppressWarnings("unchecked")
+	@Inject(method = "register", at = @At("RETURN"), remap = false)
+	private <V extends IForgeRegistryEntry<V>> void register(V value, CallbackInfo ci)
+    {
+		if (this == ForgeRegistries.BLOCKS && AdditionalPlacementsMod.dynamicRegistration)
+		{
+			Block block = (Block) value;
+			Registration.tryApply(block, block.getRegistryName(), (blockId, obj) -> ((IForgeRegistry<Block>) this).register(obj));
+		}
+    }
+}
