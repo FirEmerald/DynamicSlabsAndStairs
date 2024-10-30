eada99878a66996d9730c2feed15c24c149eaaea
FirEmerald <mccaslinwalt@gmail.com>
2024-10-29 19:45:19 -0500

    Make Generation Types Safer
    Made type construction method private
    -Mods must register generators instead
    made fields in APGenerationTypes private non-final and added accesors

--- src/main/java/com/firemerald/additionalplacements/block/interfaces/IStairBlock.java
+++ src/main/java/com/firemerald/additionalplacements/block/interfaces/IStairBlock.java
@@ -313,7 +313,7 @@ public interface IStairBlock<T extends Block> extends IPlacementBlock<T>
     
 	@Override
 	public default GenerationType<?, ?> getGenerationType() {
-		return APGenerationTypes.STAIRS;
+		return APGenerationTypes.stairs();
 	}
 
 	public default ComplexFacing getFacing(BlockHitResult hitResult)
--- src/main/java/com/firemerald/additionalplacements/common/CommonModEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/common/CommonModEventHandler.java
@@ -30,13 +30,14 @@ import net.minecraftforge.registries.NewRegistryEvent;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CommonModEventHandler
 {
-	public static boolean lockGenerationTypeRegistry = false;
+	private static boolean init = false;
 	
 	@SubscribeEvent
 	public static void onNewRegistry(NewRegistryEvent event) { //best hook I could find for loading a config after all mods have been processed but before registries are built
-		if (!lockGenerationTypeRegistry) {
-			lockGenerationTypeRegistry = true;
+		if (!init) {
+			Registration.registerTypes();
 			APConfigs.init();
+			init = true;
 		}
 	}
 	
