b1ac46668160a9173566778fa03da312380aa7a8
FirEmerald <mccaslinwalt@gmail.com>
2024-10-30 21:57:05 -0500

    Generation Types improvements
    Removed unnecessary protectionKey object
    Merged GenerationTypeConstructor into GenerationType
    enabledForBlock now also accepts the block instance
    Constructor field generices now use super/extends to be more flexible
    Generation types construct with the builder instance directly instead of it's fields
    Made BuilderBase classes protected

--- src/main/java/com/firemerald/additionalplacements/generation/SimpleGenerationType.java
+++ src/main/java/com/firemerald/additionalplacements/generation/SimpleGenerationType.java
@@ -3,16 +3,15 @@ package com.firemerald.additionalplacements.generation;
 import java.util.function.Function;
 
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
-import com.firemerald.additionalplacements.config.GenerationBlacklist;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
 public class SimpleGenerationType<T extends Block, U extends AdditionalPlacementBlock<T>> extends GenerationType<T, U> {
-	public abstract static class BuilderBase<T extends Block, U extends AdditionalPlacementBlock<T>, V extends SimpleGenerationType<T, U>, W extends BuilderBase<T, U, V, W>> extends GenerationType.BuilderBase<T, U, V, W> {
-		protected Function<T, U> constructor;
+	protected abstract static class BuilderBase<T extends Block, U extends AdditionalPlacementBlock<T>, V extends SimpleGenerationType<T, U>, W extends BuilderBase<T, U, V, W>> extends GenerationType.BuilderBase<T, U, V, W> {
+		protected Function<? super T, ? extends U> constructor;
 		
-		public W constructor(Function<T, U> constructor) {
+		public W constructor(Function<? super T, ? extends U> constructor) {
 			this.constructor = constructor;
 			return me();
 		}
@@ -20,16 +19,16 @@ public class SimpleGenerationType<T extends Block, U extends AdditionalPlacement
 	
 	public static class Builder<T extends Block, U extends AdditionalPlacementBlock<T>> extends BuilderBase<T, U, SimpleGenerationType<T, U>, Builder<T, U>> {
 		@Override
-		public SimpleGenerationType<T, U> construct(Object protectionKey, ResourceLocation name, String description) {
-			return new SimpleGenerationType<>(protectionKey, name, description, constructor, blacklist, placementEnabled);
+		public SimpleGenerationType<T, U> construct(ResourceLocation name, String description) {
+			return new SimpleGenerationType<>(name, description, this);
 		}
 	}
 	
-	private final Function<T, U> constructor;
+	private final Function<? super T, ? extends U> constructor;
 
-	protected SimpleGenerationType(Object protectionKey, ResourceLocation name, String description, Function<T, U> constructor, GenerationBlacklist blacklist, boolean placementEnabled) {
-		super(protectionKey, name, description, blacklist, placementEnabled);
-		this.constructor = constructor;
+	protected SimpleGenerationType(ResourceLocation name, String description, BuilderBase<T, U, ?, ?> builder) {
+		super(name, description, builder);
+		this.constructor = builder.constructor;
 	}
 
 	@Override
--- src/main/java/com/firemerald/additionalplacements/generation/SimpleRotatableGenerationType.java
+++ src/main/java/com/firemerald/additionalplacements/generation/SimpleRotatableGenerationType.java
@@ -1,18 +1,15 @@
 package com.firemerald.additionalplacements.generation;
 
-import java.util.function.Function;
-
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
 import com.firemerald.additionalplacements.block.interfaces.ISimpleRotationBlock;
 import com.firemerald.additionalplacements.config.BlockBlacklist;
-import com.firemerald.additionalplacements.config.GenerationBlacklist;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class SimpleRotatableGenerationType<T extends Block, U extends AdditionalPlacementBlock<T> & ISimpleRotationBlock> extends SimpleGenerationType<T, U> {
-	public abstract static class BuilderBase<T extends Block, U extends AdditionalPlacementBlock<T> & ISimpleRotationBlock, V extends SimpleRotatableGenerationType<T, U>, W extends BuilderBase<T, U, V, W>> extends SimpleGenerationType.BuilderBase<T, U, V, W> {
+	protected abstract static class BuilderBase<T extends Block, U extends AdditionalPlacementBlock<T> & ISimpleRotationBlock, V extends SimpleRotatableGenerationType<T, U>, W extends BuilderBase<T, U, V, W>> extends SimpleGenerationType.BuilderBase<T, U, V, W> {
 		protected BlockBlacklist 
 		logicRotationBlacklist = new BlockBlacklist.Builder().build(),
 		textureRotationBlacklist = new BlockBlacklist.Builder().build(),
@@ -36,18 +33,18 @@ public class SimpleRotatableGenerationType<T extends Block, U extends Additional
 	
 	public static class Builder<T extends Block, U extends AdditionalPlacementBlock<T> & ISimpleRotationBlock> extends BuilderBase<T, U, SimpleRotatableGenerationType<T, U>, Builder<T, U>> {
 		@Override
-		public SimpleRotatableGenerationType<T, U> construct(Object protectionKey, ResourceLocation name, String description) {
-			return new SimpleRotatableGenerationType<>(protectionKey, name, description, constructor, blacklist, placementEnabled, logicRotationBlacklist, textureRotationBlacklist, modelRotationBlacklist);
+		public SimpleRotatableGenerationType<T, U> construct(ResourceLocation name, String description) {
+			return new SimpleRotatableGenerationType<>(name, description, this);
 		}
 	}
 	
 	private final BlockBlacklist logicRotationBlackist, textureRotationBlacklist, modelRotationBlacklist;
 	
-	protected SimpleRotatableGenerationType(Object protectionKey, ResourceLocation name, String description, Function<T, U> constructor, GenerationBlacklist blacklist, boolean placementEnabled, BlockBlacklist logicRotationBlackist, BlockBlacklist textureRotationBlacklist, BlockBlacklist modelRotationBlacklist) {
-		super(protectionKey, name, description, constructor, blacklist, placementEnabled);
-		this.logicRotationBlackist = logicRotationBlackist;
-		this.textureRotationBlacklist = textureRotationBlacklist;
-		this.modelRotationBlacklist = modelRotationBlacklist;
+	protected SimpleRotatableGenerationType(ResourceLocation name, String description, BuilderBase<T, U, ?, ?> builder) {
+		super(name, description, builder);
+		this.logicRotationBlackist = builder.logicRotationBlacklist;
+		this.textureRotationBlacklist = builder.textureRotationBlacklist;
+		this.modelRotationBlacklist = builder.modelRotationBlacklist;
 	}
 
 	@Override
