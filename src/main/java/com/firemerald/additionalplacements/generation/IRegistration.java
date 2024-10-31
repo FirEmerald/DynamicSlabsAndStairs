cf1f63905001d8fd75db074a005219692b065a07
FirEmerald <mccaslinwalt@gmail.com>
2024-10-30 21:57:13 -0500

    Generation Types improvements
    Removed unnecessary protectionKey object
    Merged GenerationTypeConstructor into GenerationType
    enabledForBlock now also accepts the block instance
    Constructor field generices now use super/extends to be more flexible
    Generation types construct with the builder instance directly instead of it's fields
    Made BuilderBase classes protected

--- src/main/java/com/firemerald/additionalplacements/generation/IRegistration.java
+++ src/main/java/com/firemerald/additionalplacements/generation/IRegistration.java
@@ -1,11 +1,12 @@
 package com.firemerald.additionalplacements.generation;
 
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
+import com.firemerald.additionalplacements.generation.GenerationType.BuilderBase;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
 @FunctionalInterface
 public interface IRegistration {
-	public <T extends Block, U extends AdditionalPlacementBlock<T>, V extends GenerationType<T, U>> V registerType(Class<T> clazz, ResourceLocation name, String description, GenerationTypeConstructor<V> typeConstructor);
+	public <T extends Block, U extends AdditionalPlacementBlock<T>, V extends GenerationType<T, U>> V registerType(Class<T> clazz, ResourceLocation name, String description, BuilderBase<T, U, V, ?> builder);
 }
