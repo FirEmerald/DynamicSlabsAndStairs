b6f463476d13abd34f7dd8b5595b4d576eeb2a61
FirEmerald <mccaslinwalt@gmail.com>
2024-11-03 15:20:21 -0600

    Final fixes for 1.20.2 fabric
    re-add model_materials for compiletime
    mixin method to get integer offset of a VertexFormatElement from a VertexFormat

---
+++ src/main/java/com/firemerald/additionalplacements/client/IVertexFormatExtensions.java
@@ -0,0 +1,7 @@
+package com.firemerald.additionalplacements.client;
+
+import com.mojang.blaze3d.vertex.VertexFormatElement;
+
+public interface IVertexFormatExtensions {
+	public int getIntOffset(VertexFormatElement element);
+}
