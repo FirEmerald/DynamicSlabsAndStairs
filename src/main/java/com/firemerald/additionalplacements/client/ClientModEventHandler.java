fe6bab66a718792686b67f88806126705131e215
FirEmerald <mccaslinwalt@gmail.com>
2024-11-04 10:27:53 -0600

    Fix not clearing model cache on resource reload

--- src/main/java/com/firemerald/additionalplacements/client/ClientModEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/client/ClientModEventHandler.java
@@ -1,6 +1,8 @@
 package com.firemerald.additionalplacements.client;
 
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
+import com.firemerald.additionalplacements.client.models.BakedRetexturedBlockModel;
+import com.firemerald.additionalplacements.client.models.BakedRotatedBlockModel;
 import com.firemerald.additionalplacements.client.models.PlacementBlockModelLoader;
 
 import net.minecraft.network.chat.MutableComponent;
@@ -9,10 +11,12 @@ import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackCompatibility;
 import net.minecraft.server.packs.repository.PackSource;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ModelEvent.RegisterGeometryLoaders;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.event.AddPackFindersEvent;
@@ -62,5 +66,13 @@ public class ClientModEventHandler
     public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event)
     {
     	event.register(APClientData.AP_PLACEMENT_KEY);
+    }
+    
+    @SubscribeEvent
+    public static void onRegisterClientReloadListeners(RegisterClientReloadListenersEvent event) {
+    	event.registerReloadListener((ResourceManagerReloadListener) resourceManager -> {
+    		BakedRetexturedBlockModel.clearCache();
+    		BakedRotatedBlockModel.clearCache();
+    	});
     }
 }
