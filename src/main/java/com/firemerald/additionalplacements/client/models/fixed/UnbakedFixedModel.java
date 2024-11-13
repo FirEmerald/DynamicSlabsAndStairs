64357da5368efa865fb51876d9604329b4e8bb39
FirEmerald <mccaslinwalt@gmail.com>
2024-11-13 17:12:10 -0600

    Model baking improvements
    Properly utilize ModelBakery.bake to use the main model cache
    Move BakedFixedModel cache to UnbakedFixedModel
    Remove other internal block model caches

--- src/main/java/com/firemerald/additionalplacements/client/models/fixed/UnbakedFixedModel.java
+++ src/main/java/com/firemerald/additionalplacements/client/models/fixed/UnbakedFixedModel.java
@@ -1,9 +1,11 @@
 package com.firemerald.additionalplacements.client.models.fixed;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
-import com.firemerald.additionalplacements.client.models.BlockModelCache;
+import com.firemerald.additionalplacements.client.models.Unwrapper;
 import com.firemerald.additionalplacements.client.models.IUnbakedGeometry;
 import com.firemerald.additionalplacements.util.BlockRotation;
 
@@ -19,7 +21,6 @@ public class UnbakedFixedModel implements IUnbakedGeometry<UnbakedFixedModel>
 	public final ResourceLocation ourModelLocation;
 	public final ModelResourceLocation theirModelLocation;
 	public final BlockRotation modelRotation;
-	private UnbakedModel ourModel, theirModel;
 
 	public UnbakedFixedModel(AdditionalPlacementBlock<?> block, ResourceLocation ourModelLocation, ModelResourceLocation theirModelLocation, BlockRotation modelRotation)
 	{
@@ -32,8 +33,8 @@ public class UnbakedFixedModel implements IUnbakedGeometry<UnbakedFixedModel>
 	@Override
 	public void resolveParents(Function<ResourceLocation, UnbakedModel> modelGetter, BlockModel context)
 	{
-		ourModel = modelGetter.apply(ourModelLocation);
-		theirModel = modelGetter.apply(theirModelLocation);
+		modelGetter.apply(ourModelLocation);
+		modelGetter.apply(theirModelLocation);
 	}
 
 	@Override
@@ -41,9 +42,21 @@ public class UnbakedFixedModel implements IUnbakedGeometry<UnbakedFixedModel>
 			BlockModel context, ModelBaker bakery, Function<Material, TextureAtlasSprite> spriteGetter,
 			ModelState modelTransform, ItemOverrides overrides, ResourceLocation modelLocation)
 	{
-		return BlockModelCache.bake(block, 
-				BlockModelCache.bake(this.ourModel, bakery, spriteGetter, modelTransform, ourModelLocation), 
-				BlockModelCache.bake(this.theirModel, bakery, spriteGetter, theirModelLocation),
+		return get(block, 
+				Unwrapper.unwrap(bakery.bake(ourModelLocation, modelTransform)), 
+				Unwrapper.unwrap(bakery.bake(theirModelLocation, BlockModelRotation.X0_Y0)), 
 				modelRotation);
+	}
+	
+	private static record ModelKey(AdditionalPlacementBlock<?> block, BakedModel ourModel, BakedModel theirModel, BlockRotation modelRotation) {}
+	
+	private static final Map<ModelKey, BakedFixedModel> MODEL_CACHE = new HashMap<>();
+	
+	public static BakedFixedModel get(AdditionalPlacementBlock<?> block, BakedModel ourModel, BakedModel theirModel, BlockRotation modelRotation) {
+		return MODEL_CACHE.computeIfAbsent(new ModelKey(block, ourModel, theirModel, modelRotation), key -> new BakedFixedModel(block, ourModel, theirModel, modelRotation));
+	}
+	
+	public static void clearCache() {
+		MODEL_CACHE.clear();
 	}
 }
