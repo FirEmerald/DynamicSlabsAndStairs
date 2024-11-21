8c37a20eb4de95331032c6bafc315406d6ee26fc
FirEmerald <mccaslinwalt@gmail.com>
2024-11-20 18:49:39 -0600

    Another model system overhaul
    Replace old model loading system with an injection that creates UnbakedModel instances to replace missing model entries after blockstate json has been parsed
    - Uses less RAM than the previous method without sacrificing functionality
    - No longer requires a built-in resourcepack to supply dynamic resources
    - No longer requires a model loader
    Removed basic models system

--- src/main/java/com/firemerald/additionalplacements/client/ClientModEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/client/ClientModEventHandler.java
@@ -1,35 +1,21 @@
 package com.firemerald.additionalplacements.client;
 
-import java.util.List;
-
 import com.firemerald.additionalplacements.AdditionalPlacementsMod;
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
 import com.firemerald.additionalplacements.client.models.*;
-import com.firemerald.additionalplacements.client.models.dynamic.DynamicModelLoader;
-import com.firemerald.additionalplacements.client.models.fixed.FixedModelLoader;
-import com.firemerald.additionalplacements.client.models.fixed.UnbakedFixedModel;
-import com.firemerald.additionalplacements.client.resources.APDynamicResources;
+import com.firemerald.additionalplacements.generation.GenerationType;
+import com.firemerald.additionalplacements.generation.Registration;
 
 import me.pepperbell.continuity.client.model.CtmBakedModel;
 import me.pepperbell.continuity.client.model.EmissiveBakedModel;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackResources;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.repository.Pack;
-import net.minecraft.server.packs.repository.Pack.Info;
-import net.minecraft.server.packs.repository.Pack.ResourcesSupplier;
-import net.minecraft.server.packs.repository.PackCompatibility;
-import net.minecraft.server.packs.repository.PackSource;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
-import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ModelEvent.RegisterGeometryLoaders;
+import net.minecraftforge.client.event.ModelEvent.RegisterAdditional;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -41,44 +27,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 @OnlyIn(Dist.CLIENT)
 public class ClientModEventHandler
 {
-	public static final Pack GENERATED_RESOURCES_PACK = Pack.create(
-			"Additional Placements dynamic resources",
-			Component.literal("title"),
-			true,
-			new ResourcesSupplier() { //TODO
-
-				@Override
-				public PackResources openPrimary(String p_298664_)
-				{
-					return new APDynamicResources();
-				}
-
-				@Override
-				public PackResources openFull(String p_251717_, Info p_298253_)
-				{
-					return new APDynamicResources();
-				}
-
-			},
-			new Pack.Info(Component.literal("description"), PackCompatibility.COMPATIBLE, FeatureFlagSet.of(), List.of()),
-			Pack.Position.BOTTOM,
-			true,
-			PackSource.BUILT_IN
-			);
-
-	@SubscribeEvent
-	public static void onAddPackFinders(AddPackFindersEvent event)
-	{
-		if (event.getPackType() == PackType.CLIENT_RESOURCES) event.addRepositorySource(addPack -> addPack.accept(GENERATED_RESOURCES_PACK));
-	}
-
-	@SubscribeEvent
-	public static void onModelRegistryEvent(RegisterGeometryLoaders event)
-	{
-		event.register(FixedModelLoader.ID.getPath(), new FixedModelLoader());
-		event.register(DynamicModelLoader.ID.getPath(), new DynamicModelLoader());
-	}
-
     @SubscribeEvent(priority = EventPriority.LOWEST)
 	public static void onRegisterBlockColorHandlers(RegisterColorHandlersEvent.Block event)
     {
@@ -94,7 +42,7 @@ public class ClientModEventHandler
     @SubscribeEvent
     public static void onRegisterClientReloadListeners(RegisterClientReloadListenersEvent event) {
     	event.registerReloadListener((ResourceManagerReloadListener) resourceManager -> {
-    		UnbakedFixedModel.clearCache();
+    		BakedPlacementModel.clearCache();
     	});
     }
 
@@ -108,5 +56,13 @@ public class ClientModEventHandler
     			else return null;
     		});
     	}
+    }
+    
+    @SubscribeEvent
+    public static void onRegisterAdditionalModels(RegisterAdditional event) {
+    	Registration.types()
+    	.flatMap(GenerationType::created)
+    	.flatMap(entry -> entry.newBlock().allBaseModels())
+    	.forEach(event::register);
     }
 }
---
+++ src/main/java/com/firemerald/additionalplacements/mixin/MixinModelBakery.java
@@ -0,0 +1,64 @@
+package com.firemerald.additionalplacements.mixin;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
+import com.firemerald.additionalplacements.client.models.PlacementModelState;
+import com.firemerald.additionalplacements.client.models.UnbakedPlacementModel;
+import com.firemerald.additionalplacements.client.models.definitions.StateModelDefinition;
+import com.firemerald.additionalplacements.generation.GenerationType;
+import com.firemerald.additionalplacements.generation.Registration;
+import com.firemerald.additionalplacements.util.BlockRotation;
+
+import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.client.renderer.block.model.BlockModel;
+import net.minecraft.client.resources.model.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.block.state.BlockState;
+
+@Mixin(ModelBakery.class)
+public class MixinModelBakery {
+	@Shadow
+	@Final
+	public Map<ResourceLocation, UnbakedModel> topLevelModels;
+	@Shadow
+	@Final
+    private Map<ResourceLocation, UnbakedModel> unbakedCache;
+	
+	@Inject(method = "<init>", at = @At("RETURN"))
+	public void init(BlockColors blockColors, ProfilerFiller profilerFiller, Map<ResourceLocation, BlockModel> modelResources, Map<ResourceLocation, List<ModelBakery.LoadedJson>> blockStateResources, CallbackInfo cli) {
+		Map<UnbakedModel, ResourceLocation> topLevelModelsReversed = topLevelModels.entrySet().stream().collect(Collectors.toMap(Entry::getValue, Entry::getKey, (o, n) -> o));
+		UnbakedModel missingModel = unbakedCache.get(ModelBakery.MISSING_MODEL_LOCATION);
+		//TODO replace with hook in net.minecraft.client.resources.model.BlockStateModelLoader.loadBlockStateDefinitions(ResourceLocation, StateDefinition) line 166 at BlockStateModelLoader.LoadedModel loadedModel = (BlockStateModelLoader.LoadedModel)map2.get(blockState);
+		Registration.types().flatMap(GenerationType::created).forEach(entry -> {
+			AdditionalPlacementBlock<?> block = entry.newBlock();
+			block.getStateDefinition().getPossibleStates().forEach(ourState -> {
+				ModelResourceLocation ourModelLocation = BlockModelShaper.stateToModelLocation(ourState);
+				if (topLevelModels.get(ourModelLocation) == missingModel) {
+					BlockState theirState = block.getModelState(ourState);
+					StateModelDefinition modelDefinition = block.getModelDefinition(ourState);
+					ResourceLocation ourModel = modelDefinition.location(block.getBaseModelPrefix());
+					ModelState ourModelRotation = PlacementModelState.by(modelDefinition.xRotation(), modelDefinition.yRotation());
+					UnbakedModel theirModel = topLevelModels.getOrDefault(BlockModelShaper.stateToModelLocation(theirState), missingModel);
+					BlockRotation theirModelRotation = block.getRotation(ourState);
+					UnbakedPlacementModel unbakedModel = UnbakedPlacementModel.of(block, ourModel, ourModelRotation, topLevelModelsReversed.get(theirModel), theirModel, theirModelRotation);
+					topLevelModels.put(ourModelLocation, unbakedModel);
+					unbakedCache.put(ourModelLocation, unbakedModel);
+				}
+			});
+		});
+		UnbakedPlacementModel.clearCache();
+	}
+}
