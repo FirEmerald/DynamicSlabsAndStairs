a056764efb03dbda4d8eed93188bce8a2cf1325b
FirEmerald <mccaslinwalt@gmail.com>
2024-11-20 18:47:58 -0600

    Another model system overhaul
    Replace old model loading system with an injection that creates UnbakedModel instances to replace missing model entries after blockstate json has been parsed
    - Uses less RAM than the previous method without sacrificing functionality
    - No longer requires a built-in resourcepack to supply dynamic resources
    - No longer requires a model loader
    Removed basic models system

--- src/main/java/com/firemerald/additionalplacements/client/ClientModEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/client/ClientModEventHandler.java
@@ -1,25 +1,17 @@
 package com.firemerald.additionalplacements.client;
 
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
-import com.firemerald.additionalplacements.client.models.dynamic.DynamicModelLoader;
-import com.firemerald.additionalplacements.client.models.fixed.FixedModelLoader;
-import com.firemerald.additionalplacements.client.resources.APDynamicResources;
+import com.firemerald.additionalplacements.client.models.BakedPlacementModel;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.repository.Pack;
-import net.minecraft.server.packs.repository.PackCompatibility;
-import net.minecraft.server.packs.repository.PackSource;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.event.AddPackFindersEvent;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -30,32 +22,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 @OnlyIn(Dist.CLIENT)
 public class ClientModEventHandler
 {
-	public static final Pack GENERATED_RESOURCES_PACK = new Pack(
-			"Additional Placements blockstate redirection pack",
-			true,
-			APDynamicResources::new,
-			new TextComponent("title"),
-			new TextComponent("description"),
-			PackCompatibility.COMPATIBLE,
-			Pack.Position.BOTTOM,
-			true,
-			PackSource.BUILT_IN,
-			true
-			);
-
-	@SubscribeEvent
-	public static void onAddPackFinders(AddPackFindersEvent event)
-	{
-		if (event.getPackType() == PackType.CLIENT_RESOURCES) event.addRepositorySource((addPack, buildPack) -> addPack.accept(GENERATED_RESOURCES_PACK));
-	}
-
-	@SubscribeEvent
-	public static void onModelRegistryEvent(ModelRegistryEvent event)
-	{
-		ModelLoaderRegistry.registerLoader(FixedModelLoader.ID, new FixedModelLoader());
-		ModelLoaderRegistry.registerLoader(DynamicModelLoader.ID, new DynamicModelLoader());
-	}
-
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void init(FMLClientSetupEvent event) {
     	ForgeRegistries.BLOCKS.forEach(block -> {
@@ -67,5 +33,12 @@ public class ClientModEventHandler
     	});
     	Minecraft.getInstance().getBlockColors().register(new AdditionalBlockColor(), ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof AdditionalPlacementBlock && !((AdditionalPlacementBlock<?>) block).hasCustomColors()).toArray(Block[]::new));
     	ClientRegistry.registerKeyBinding(APClientData.AP_PLACEMENT_KEY);
+    }
+
+    @SubscribeEvent
+    public static void onRegisterClientReloadListeners(RegisterClientReloadListenersEvent event) {
+    	event.registerReloadListener((ResourceManagerReloadListener) resourceManager -> {
+    		BakedPlacementModel.clearCache();
+    	});
     }
 }
--- src/main/java/com/firemerald/additionalplacements/datagen/ModelGenerator.java
+++ src/main/java/com/firemerald/additionalplacements/datagen/ModelGenerator.java
@@ -3,7 +3,6 @@ package com.firemerald.additionalplacements.datagen;
 import com.firemerald.additionalplacements.client.models.definitions.*;
 
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -18,19 +17,15 @@ public class ModelGenerator extends BlockStateProvider
 	protected void registerStatesAndModels()
 	{
 		new SlabModelsBuilder(this)
-		.set(SlabModels.DYNAMIC_MODEL_FOLDER).addAction((builder, model) -> builder.customLoader(DynamicModelLoaderBuilder::new).setModel(new ResourceLocation(SlabModels.BASE_MODEL_FOLDER.getNamespace(), SlabModels.BASE_MODEL_FOLDER.getPath() + model))).compile() //build dynamic
 		.set(SlabModels.SIDE_ALL_MODEL_FOLDER, SlabModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("side", "#all").texture("top", "#all").texture("bottom", "#all")).compile() //build side_all
 		.set(SlabModels.COLUMN_MODEL_FOLDER, SlabModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("top", "#end").texture("bottom", "#end")).compile(); //build pillar
 		new StairModelsBuilder(this)
-		.set(StairModels.DYNAMIC_MODEL_FOLDER).addAction((builder, model) -> builder.customLoader(DynamicModelLoaderBuilder::new).setModel(new ResourceLocation(StairModels.BASE_MODEL_FOLDER.getNamespace(), StairModels.BASE_MODEL_FOLDER.getPath() + model))).compile() //build dynamic
 		.set(StairModels.SIDE_ALL_MODEL_FOLDER, StairModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("side", "#all").texture("top", "#all").texture("bottom", "#all")).compile() //build side_all
 		.set(StairModels.COLUMN_MODEL_FOLDER, StairModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("top", "#end").texture("bottom", "#end")).compile(); //build pillar
 		new CarpetModelsBuilder(this)
-		.set(CarpetModels.DYNAMIC_MODEL_FOLDER).addAction((builder, model) -> builder.customLoader(DynamicModelLoaderBuilder::new).setModel(new ResourceLocation(CarpetModels.BASE_MODEL_FOLDER.getNamespace(), CarpetModels.BASE_MODEL_FOLDER.getPath() + model))).compile() //build dynamic
 		.set(CarpetModels.SIDE_ALL_MODEL_FOLDER, CarpetModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("side", "#all").texture("top", "#all").texture("bottom", "#all")).compile() //build side_all
 		.set(CarpetModels.COLUMN_MODEL_FOLDER, CarpetModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("top", "#end").texture("bottom", "#end")).compile(); //build pillar
 		new PressurePlateModelsBuilder<>(this)
-		.set(PressurePlateModels.DYNAMIC_MODEL_FOLDER).addAction((builder, model) -> builder.customLoader(DynamicModelLoaderBuilder::new).setModel(new ResourceLocation(PressurePlateModels.BASE_MODEL_FOLDER.getNamespace(), PressurePlateModels.BASE_MODEL_FOLDER.getPath() + model))).compile() //build dynamic
 		.set(PressurePlateModels.SIDE_ALL_MODEL_FOLDER, PressurePlateModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("side", "#all").texture("top", "#all").texture("bottom", "#all")).compile() //build side_all
 		.set(PressurePlateModels.COLUMN_MODEL_FOLDER, PressurePlateModels.BASE_MODEL_FOLDER).addAction((builder, model) -> builder.texture("top", "#end").texture("bottom", "#end")).compile(); //build pillar
 	}
