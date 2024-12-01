83ff537703332d5a742b6e378403cb5cd5c676e7
FirEmerald <mccaslinwalt@gmail.com>
2024-12-01 15:50:05 -0600

    Improved model registration hook
    prevent additional placement blocks from printing missing blockstate json warnings
    unbaked models are created during model fetching - prevents missing model variant warnings

--- src/main/java/com/firemerald/additionalplacements/block/AdditionalPlacementBlock.java
+++ src/main/java/com/firemerald/additionalplacements/block/AdditionalPlacementBlock.java
@@ -88,7 +88,7 @@ public abstract class AdditionalPlacementBlock<T extends Block> extends Block im
 		return to;
 	}
 	
-	protected boolean isValidProperty(Property<?> prop) {
+	public boolean isValidProperty(Property<?> prop) {
 		return true;
 	}
 
@@ -451,12 +451,4 @@ public abstract class AdditionalPlacementBlock<T extends Block> extends Block im
 
 	@OnlyIn(Dist.CLIENT)
 	public abstract StateModelDefinition getModelDefinition(BlockState state);
-
-	@OnlyIn(Dist.CLIENT)
-	public abstract Stream<StateModelDefinition> allModelDefinitions();
-
-	@OnlyIn(Dist.CLIENT)
-	public Stream<ResourceLocation> allBaseModels() {
-		return allModelDefinitions().map(model -> model.location(getBaseModelPrefix()));
-	}
 }
--- src/main/java/com/firemerald/additionalplacements/mixin/MixinModelBakery.java
+++ src/main/java/com/firemerald/additionalplacements/mixin/MixinModelBakery.java
@@ -1,72 +1,101 @@
 package com.firemerald.additionalplacements.mixin;
 
-import java.util.HashSet;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.Map.Entry;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
 import com.firemerald.additionalplacements.client.models.PlacementModelState;
 import com.firemerald.additionalplacements.client.models.UnbakedPlacementModel;
 import com.firemerald.additionalplacements.client.models.definitions.StateModelDefinition;
-import com.firemerald.additionalplacements.generation.Registration;
 import com.firemerald.additionalplacements.util.BlockRotation;
+import com.mojang.datafixers.util.Pair;
 
+import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.block.BlockModelShaper;
 import net.minecraft.client.resources.model.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mixin(ModelBakery.class)
 public class MixinModelBakery {
 	@Shadow
 	@Final
-	public Map<ResourceLocation, UnbakedModel> topLevelModels;
+	private BlockColors blockColors;
 	@Shadow
-	@Final
-    private Map<ResourceLocation, UnbakedModel> unbakedCache;
+	public UnbakedModel getModel(ResourceLocation modelLocation) {
+		return null;
+	}
 	
 	@Inject(method = "processLoading", at = @At("RETURN"))
 	public void processLoading(ProfilerFiller profiler, int maxMipmapLevel, CallbackInfo cli) {
-		Map<UnbakedModel, ResourceLocation> topLevelModelsReversed = topLevelModels.entrySet().stream().collect(Collectors.toMap(Entry::getValue, Entry::getKey, (o, n) -> o));
-		UnbakedModel missingModel = unbakedCache.get(ModelBakery.MISSING_MODEL_LOCATION);
-		//TODO replace with hook in net.minecraft.client.resources.model.BlockStateModelLoader.loadBlockStateDefinitions(ResourceLocation, StateDefinition) line 166 at BlockStateModelLoader.LoadedModel loadedModel = (BlockStateModelLoader.LoadedModel)map2.get(blockState);
-		Registration.forEachCreated(entry -> {
-			AdditionalPlacementBlock<?> block = entry.newBlock();
-			block.getStateDefinition().getPossibleStates().forEach(ourState -> {
-				ModelResourceLocation ourModelLocation = BlockModelShaper.stateToModelLocation(ourState);
-				if (topLevelModels.get(ourModelLocation) == missingModel) {
-					BlockState theirState = block.getModelState(ourState);
-					StateModelDefinition modelDefinition = block.getModelDefinition(ourState);
-					ResourceLocation ourModel = modelDefinition.location(block.getBaseModelPrefix());
-					ModelState ourModelRotation = PlacementModelState.by(modelDefinition.xRotation(), modelDefinition.yRotation());
-					UnbakedModel theirModel = topLevelModels.getOrDefault(BlockModelShaper.stateToModelLocation(theirState), missingModel);
-					BlockRotation theirModelRotation = block.getRotation(ourState);
-					UnbakedPlacementModel unbakedModel = UnbakedPlacementModel.of(block, ourModel, ourModelRotation, topLevelModelsReversed.get(theirModel), theirModel, theirModelRotation);
-					topLevelModels.put(ourModelLocation, unbakedModel);
-					unbakedCache.put(ourModelLocation, unbakedModel);
-				}
-			});
-		});
 		UnbakedPlacementModel.clearCache();
 	}
+
+	@Redirect(
+			method = "loadModel", 
+			at = @At(value = "INVOKE", target = "org/slf4j/Logger.warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"), 
+			slice = @Slice(
+					from = @At(value = "CONSTANT", args = {"stringValue=Exception loading blockstate definition: {}: {}"}),
+					to = @At(value = "INVOKE", target = "org/slf4j/Logger.warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", shift = Shift.AFTER)
+					)
+			)
+	private void loadModel(Logger logger, String message, Object blockStateJsonObj, Object eObj, ResourceLocation blockstateLocation) {
+		if (eObj instanceof FileNotFoundException) {
+			ResourceLocation blockId = new ResourceLocation(blockstateLocation.getNamespace(), blockstateLocation.getPath());
+			Block block = ForgeRegistries.BLOCKS.getValue(blockId);
+			if (block instanceof AdditionalPlacementBlock) return;
+		}
+		logger.warn(message, blockStateJsonObj, eObj);
+	}
 	
-	@Inject(method = "getSpecialModels", at = @At("RETURN"))
-	public void getSpecialModels(CallbackInfoReturnable<Set<ResourceLocation>> cli) {
-		Set<ResourceLocation> set = new HashSet<>(cli.getReturnValue());
-    	Registration.created()
-    	.flatMap(entry -> entry.newBlock().allBaseModels())
-    	.forEach(set::add);
-    	cli.setReturnValue(set);
+	@ModifyVariable(
+			method = "lambda$loadModel$25", 
+			at = @At("STORE"), 
+			index = 7
+			)
+	private Pair<UnbakedModel, Supplier<ModelBakery.ModelGroupKey>> loadModelLambda(
+			Pair<UnbakedModel, Supplier<ModelBakery.ModelGroupKey>> modelPair, 
+			Map<ModelResourceLocation, BlockState> modelToState, 
+			ResourceLocation blockId, 
+			Pair<UnbakedModel, Supplier<ModelBakery.ModelGroupKey>> missingModelPair, 
+			Map<ModelBakery.ModelGroupKey, Set<BlockState>> modelGroups, 
+			ModelResourceLocation currentModelLocation, 
+			BlockState ourState) {
+		if (modelPair == null) { //replace only states which do not already have a model
+			if (ourState != null && ourState.getBlock() instanceof AdditionalPlacementBlock) {
+				AdditionalPlacementBlock<?> block = (AdditionalPlacementBlock<?>) ourState.getBlock();
+
+				BlockState theirState = block.getModelState(ourState);
+				StateModelDefinition modelDefinition = block.getModelDefinition(ourState);
+				ResourceLocation ourModel = modelDefinition.location(block.getBaseModelPrefix());
+				ModelState ourModelRotation = PlacementModelState.by(modelDefinition.xRotation(), modelDefinition.yRotation());
+				ModelResourceLocation theirModelLocation = BlockModelShaper.stateToModelLocation(theirState);
+				UnbakedModel theirModel = getModel(theirModelLocation);
+				BlockRotation theirModelRotation = block.getRotation(ourState);
+				UnbakedPlacementModel unbakedModel = UnbakedPlacementModel.of(block, ourModel, ourModelRotation, theirModelLocation, theirModel, theirModelRotation);
+				
+				List<Property<?>> coloringProperties = this.blockColors.getColoringProperties(theirState.getBlock()).stream()
+						.filter(block::isValidProperty) //just in case
+						.collect(Collectors.toList());
+	            return Pair.of(unbakedModel, () -> ModelBakery.ModelGroupKey.create(ourState, unbakedModel, coloringProperties));
+			}
+		}
+		return modelPair;
 	}
 }
