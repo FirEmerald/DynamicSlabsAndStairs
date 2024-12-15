83f7f9cc23da77139e6cd488cbeb95a98a411932
FirEmerald <mccaslinwalt@gmail.com>
2024-12-14 21:50:09 -0600

    Remove leftover class

--- src/main/java/com/firemerald/additionalplacements/util/IStateDefinitionBuilderExtension.java
+++
@@ -1,9 +0,0 @@
-package com.firemerald.additionalplacements.util;
-
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.StateHolder;
-import net.minecraft.world.level.block.state.properties.Property;
-
-public interface IStateDefinitionBuilderExtension<O, S extends StateHolder<O, S>> {
-	public StateDefinition.Builder<O, S> addSingular(Property<?>... pProperties);
-}
