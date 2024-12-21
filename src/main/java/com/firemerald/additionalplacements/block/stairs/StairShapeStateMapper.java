845c88160ba86bda1a833c74a004c057b52ddd2e
FirEmerald <mccaslinwalt@gmail.com>
2024-12-20 20:16:52 -0600

    Another stair state overhaul
    stair facing and shape is now combined into a single block property, making it easier to tell what the actual placement and shape is
    added config to enable/disable correction of blockstates from older versions and from different stair shape options
    stair shape debugger command now generates the appropriate number of stair connections for a block's connection type (with the exception of complex ones having each state twice due to flipped states being equal)
    stair shape and placement properties are stored in enum-like classes that hold references to other relevant property sets for better performance when converting to/from vanilla state properties
    fixed and improved partially broken chunk loading blockstate correction code

---
+++ src/main/java/com/firemerald/additionalplacements/block/stairs/StairShapeStateMapper.java
@@ -0,0 +1,71 @@
+package com.firemerald.additionalplacements.block.stairs;
+
+import com.firemerald.additionalplacements.block.stairs.common.CommonStairShape;
+import com.firemerald.additionalplacements.block.stairs.common.CommonStairShapeState;
+import com.firemerald.additionalplacements.block.stairs.v1.V1StairShapeState;
+import com.firemerald.additionalplacements.block.stairs.v2.V2StairShapeState;
+import com.firemerald.additionalplacements.block.stairs.vanilla.VanillaStairShapeState;
+import com.firemerald.additionalplacements.util.BlockRotation;
+import com.firemerald.additionalplacements.util.ComplexFacing;
+
+import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.level.block.state.properties.StairsShape;
+
+public class StairShapeStateMapper {
+	private static boolean lock = false;
+	
+	public static void run() {
+		if (!lock) {
+			lock = true;
+			setMapping(StairsShape.STRAIGHT   , StairsShape.STRAIGHT   , CommonStairShape.STRAIGHT         , CommonStairShape.OUTER_BACK_LEFT_BOTTOM_RIGHT, CommonStairShape.OUTER_BACK_RIGHT_BOTTOM_LEFT);
+			setMapping(StairsShape.INNER_LEFT , StairsShape.INNER_RIGHT, CommonStairShape.INNER_FRONT_LEFT , CommonStairShape.INNER_TOP_LEFT              , CommonStairShape.INNER_BOTH_LEFT             );
+			setMapping(StairsShape.INNER_RIGHT, StairsShape.INNER_LEFT , CommonStairShape.INNER_FRONT_RIGHT, CommonStairShape.INNER_TOP_RIGHT             , CommonStairShape.INNER_BOTH_RIGHT            );
+			setMapping(StairsShape.OUTER_LEFT , StairsShape.OUTER_RIGHT, CommonStairShape.OUTER_BACK_LEFT  , CommonStairShape.OUTER_BOTTOM_LEFT           , CommonStairShape.OUTER_BOTH_LEFT             );
+			setMapping(StairsShape.OUTER_RIGHT, StairsShape.OUTER_LEFT , CommonStairShape.OUTER_BACK_RIGHT , CommonStairShape.OUTER_BOTTOM_RIGHT          , CommonStairShape.OUTER_BOTH_RIGHT            );
+			//V1
+			for (V1StairShapeState v1 : V1StairShapeState.values()) {
+				CommonStairShapeState common = CommonStairShapeState.of(v1.shape.placingType.fromV1Placing(v1.placing), v1.shape.equivalent);
+				v1.setCommon(common);
+			}
+			//V2
+			for (V2StairShapeState v2 : V2StairShapeState.values()) {
+				CommonStairShapeState common = CommonStairShapeState.of(v2.shape.facingType.fromV2Facing(v2.facing), v2.shape.shape);
+				v2.setCommon(common);
+			}
+		}
+	}
+	
+	private static void setMapping(StairsShape vanillaBottomShape, StairsShape vanillaTopShape, CommonStairShape commonShape, CommonStairShape... extraShapes) {
+		setMapping(vanillaBottomShape, vanillaTopShape, commonShape, true);
+		for (CommonStairShape extraShape : extraShapes) setMapping(vanillaBottomShape, vanillaTopShape, extraShape, false);
+	}
+	
+	private static void setMapping(StairsShape vanillaBottomShape, StairsShape vanillaTopShape, CommonStairShape commonShape, boolean shapesEqual) {
+		for (Direction facing : Direction.values()) if (facing.getAxis() != Axis.Y) {
+			setMapping(Axis.X, BlockRotation.X_270_Y_270, facing, Half.BOTTOM, vanillaBottomShape, Direction.EAST , commonShape, shapesEqual);
+			setMapping(Axis.X, BlockRotation.X_270_Y_270, facing, Half.TOP,    vanillaTopShape   , Direction.WEST , commonShape, shapesEqual);
+			setMapping(                                   facing, Half.BOTTOM, vanillaBottomShape, Direction.UP   , commonShape, shapesEqual);
+			setMapping(                                   facing, Half.TOP,    vanillaTopShape   , Direction.DOWN , commonShape, shapesEqual);
+			setMapping(Axis.Z, BlockRotation.X_270      , facing, Half.BOTTOM, vanillaBottomShape, Direction.SOUTH, commonShape, shapesEqual);
+			setMapping(Axis.Z, BlockRotation.X_270      , facing, Half.TOP,    vanillaTopShape   , Direction.NORTH, commonShape, shapesEqual);
+		}
+	}
+	
+	private static void setMapping(Direction vanillaFacing, Half vanillaHalf, StairsShape vanillaShape, Direction commonUp, CommonStairShape commonShape, boolean shapesEqual) {
+		VanillaStairShapeState vanilla = VanillaStairShapeState.of(vanillaFacing, vanillaHalf, vanillaShape);
+		CommonStairShapeState common = CommonStairShapeState.of(ComplexFacing.forFacing(vanillaFacing.getOpposite(), commonUp), commonShape);
+		common.setModel(vanilla, BlockRotation.IDENTITY, shapesEqual);
+		if (shapesEqual) {
+			common.setVanilla(vanilla);
+			vanilla.setCommon(common);
+		}
+	}
+	
+	private static void setMapping(Axis axis, BlockRotation rotation, Direction vanillaFacing, Half vanillaHalf, StairsShape vanillaShape, Direction commonUp, CommonStairShape commonShape, boolean shapesEqual) {
+		VanillaStairShapeState vanilla = VanillaStairShapeState.of(vanillaFacing, vanillaHalf, vanillaShape);
+		CommonStairShapeState common = CommonStairShapeState.of(ComplexFacing.forFacing(rotation.apply(vanillaFacing.getOpposite()), commonUp), commonShape);
+		common.setModel(vanilla, rotation, shapesEqual);
+	}
+}
--- src/main/java/com/firemerald/additionalplacements/generation/APGenerationTypes.java
+++ src/main/java/com/firemerald/additionalplacements/generation/APGenerationTypes.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 import com.firemerald.additionalplacements.AdditionalPlacementsMod;
 import com.firemerald.additionalplacements.block.*;
 import com.firemerald.additionalplacements.block.interfaces.ISimpleRotationBlock;
+import com.firemerald.additionalplacements.block.stairs.AdditionalStairBlock;
 import com.firemerald.additionalplacements.config.BlockBlacklist;
 import com.firemerald.additionalplacements.generation.GenerationType.BuilderBase;
 
@@ -13,7 +14,7 @@ import net.minecraft.world.level.block.*;
 
 public class APGenerationTypes implements RegistrationInitializer {
 	private static SimpleRotatableGenerationType<SlabBlock, VerticalSlabBlock> slab;
-	private static VerticalStairsGenerationType<StairBlock, VerticalStairBlock> stairs;
+	private static VerticalStairsGenerationType<StairBlock, AdditionalStairBlock> stairs;
 	private static SimpleRotatableGenerationType<CarpetBlock, AdditionalCarpetBlock> carpet;
 	private static SimpleRotatableGenerationType<PressurePlateBlock, AdditionalPressurePlateBlock> pressurePlate;
 	private static SimpleRotatableGenerationType<WeightedPressurePlateBlock, AdditionalWeightedPressurePlateBlock> weightedPressurePlate;
@@ -35,13 +36,13 @@ public class APGenerationTypes implements RegistrationInitializer {
 				.constructor(VerticalSlabBlock::of)
 				.addsProperties("axis"));
 		stairs                  = get(register, StairBlock.class                , "stairs"                 , "Stairs"                  , 
-				new VerticalStairsGenerationType.Builder<StairBlock, VerticalStairBlock>()
+				new VerticalStairsGenerationType.Builder<StairBlock, AdditionalStairBlock>()
 				.blacklistModelRotation(new BlockBlacklist.Builder()
 						.blockBlacklist(
 								"minecraft:sandstone_stairs", 
 								"minecraft:red_sandstone_stairs")
 						.build())
-				.constructor((VerticalStairsGenerationType.Constructor<StairBlock, VerticalStairBlock>) VerticalStairBlock::of));
+				.addsProperties("front_top_shape"));
 		carpet                  = get(register, CarpetBlock.class               , "carpet"                 , "Carpets"                 , AdditionalCarpetBlock::of,                "facing");
 		pressurePlate           = get(register, PressurePlateBlock.class        , "pressure_plate"         , "Regular pressure plates" , AdditionalPressurePlateBlock::of,         "facing");
 		weightedPressurePlate   = get(register, WeightedPressurePlateBlock.class, "weighted_pressure_plate", "Weighted pressure plates", AdditionalWeightedPressurePlateBlock::of, "facing");
@@ -50,7 +51,7 @@ public class APGenerationTypes implements RegistrationInitializer {
 	private static <T extends Block, U extends AdditionalPlacementBlock<T> & ISimpleRotationBlock> SimpleRotatableGenerationType<T, U> get(IRegistration register, Class<T> clazz, String name, String description, Function<? super T, ? extends U> constructor, String... addsProperties) {
 		return register.registerType(clazz, new ResourceLocation(AdditionalPlacementsMod.MOD_ID, name), description, new SimpleRotatableGenerationType.Builder<T, U>().constructor(constructor).addsProperties(addsProperties));
 	}
-	
+
 	private static <T extends Block, U extends AdditionalPlacementBlock<T>, V extends GenerationType<T, U>> V get(IRegistration register, Class<T> clazz, String name, String description, BuilderBase<T, U, V, ?> builder) {
 		return register.registerType(clazz, new ResourceLocation(AdditionalPlacementsMod.MOD_ID, name), description, builder);
 	}
@@ -59,7 +60,7 @@ public class APGenerationTypes implements RegistrationInitializer {
 		return slab;
 	}
 	
-	public static VerticalStairsGenerationType<StairBlock, VerticalStairBlock> stairs() {
+	public static VerticalStairsGenerationType<StairBlock, AdditionalStairBlock> stairs() {
 		return stairs;
 	}
 	
@@ -74,5 +75,4 @@ public class APGenerationTypes implements RegistrationInitializer {
 	public static SimpleRotatableGenerationType<WeightedPressurePlateBlock, AdditionalWeightedPressurePlateBlock> weightedPressurePlate() {
 		return weightedPressurePlate;
 	}
-	
 }
