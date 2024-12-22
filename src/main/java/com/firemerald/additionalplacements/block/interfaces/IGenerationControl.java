d1e92058a6ee83f868e6475f630ac51a4b1c0a24
FirEmerald <mccaslinwalt@gmail.com>
2024-12-21 19:42:59 -0600

    Add way for mods to blacklist blocks without a custom class

--- src/main/java/com/firemerald/additionalplacements/block/interfaces/IGenerationControl.java
+++ src/main/java/com/firemerald/additionalplacements/block/interfaces/IGenerationControl.java
@@ -1,7 +1,8 @@
 package com.firemerald.additionalplacements.block.interfaces;
 
 /**
- * A mod author may choose to add the method below (<b>with or without implementing this interface</b>) on a custom block to further control whether or not an additional placement block can be generated for it.</br>
+ * A mod author may choose to add the method below (<b>with or without implementing this interface</b>) on a custom block class to further control whether or not an additional placement block can be generated for it.</br>
+ * If looking to blacklist blocks outside of config without using a custom class, see {@link com.firemerald.additionalplacements.generation.IBlockBlacklister IBlockBlacklister} instead.
  */
 public interface IGenerationControl {
 	/**
