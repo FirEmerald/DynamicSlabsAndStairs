5b1431c23059e9571086e53c18c86ee041537ff1
FirEmerald <mccaslinwalt@gmail.com>
2024-12-22 14:03:33 -0600

    Fix non-rotated models having the wrong textures
    Fix textures being chosen by the occlusion side rather than the quad side
    Remove unneeded texture side transformer code - the side should ALWAYS be the original quad's side

--- src/main/java/com/firemerald/additionalplacements/block/interfaces/IPlacementBlock.java
+++ src/main/java/com/firemerald/additionalplacements/block/interfaces/IPlacementBlock.java
@@ -17,7 +17,6 @@ import net.minecraft.client.Camera;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -34,7 +33,6 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.model.data.ModelData;
 
 public interface IPlacementBlock<T extends Block> extends ItemLike, IGenerationControl
 {
@@ -132,12 +130,6 @@ public interface IPlacementBlock<T extends Block> extends ItemLike, IGenerationC
 
 	public default boolean enablePlacement(BlockPos hit, Level level, Direction direction, Player player) {
 		return enablePlacement(player);
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	public default Function<Direction, Direction> getModelDirectionFunction(BlockState state, RandomSource rand, ModelData extraData)
-	{
-		return Function.identity();
 	}
 	
 	public GenerationType<?, ?> getGenerationType();
