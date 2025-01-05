8d9a800ce9f3357729295cf37d48397c98c9a4ad
FirEmerald <mccaslinwalt@gmail.com>
2025-01-05 11:42:03 -0600

    Rename properties to avoid conflicts
    rename HORIZONTAL_OR_UP_FACING from "facing" to "ap_placing"
    rename HORIZONTAL_AXIS from "axis" to "ap_axis"
    Add additional helpers to IStateFixer

--- src/main/java/com/firemerald/additionalplacements/block/AdditionalBlockStateProperties.java
+++ src/main/java/com/firemerald/additionalplacements/block/AdditionalBlockStateProperties.java
@@ -1,9 +1,12 @@
 package com.firemerald.additionalplacements.block;
 
 import net.minecraft.core.Direction;
+import net.minecraft.core.Direction.Axis;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 
 public class AdditionalBlockStateProperties
 {
-	   public static final DirectionProperty HORIZONTAL_OR_UP_FACING = DirectionProperty.create("facing", Direction.UP, Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST);
+	public static final DirectionProperty HORIZONTAL_OR_UP_PLACING = DirectionProperty.create("ap_placing", Direction.UP, Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST);
+	public static final EnumProperty<Axis> HORIZONTAL_AXIS = EnumProperty.create("ap_axis", Direction.Axis.class, Direction.Axis.X, Direction.Axis.Z);
 }
