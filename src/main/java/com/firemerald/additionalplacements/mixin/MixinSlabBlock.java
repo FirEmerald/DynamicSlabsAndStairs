d8935717a4401e695401118be7e1796ced31daa1
FirEmerald <mccaslinwalt@gmail.com>
2025-02-12 13:28:53 -0600

    Fix optional mixins
    Properly set optional mixin injectors to be optional

--- src/main/java/com/firemerald/additionalplacements/mixin/MixinSlabBlock.java
+++ src/main/java/com/firemerald/additionalplacements/mixin/MixinSlabBlock.java
@@ -88,7 +88,7 @@ public abstract class MixinSlabBlock extends Block implements IVanillaSlabBlock
 		if (this.hasAdditionalStates() && enablePlacement(context.getClickedPos(), context.getLevel(), context.getClickedFace(), context.getPlayer())) ci.setReturnValue(getStateForPlacementImpl(context, ci.getReturnValue()));
 	}
 
-	@Inject(at = @At("HEAD"), remap = false, cancellable = true, target = {
+	@Inject(at = @At("HEAD"), remap = false, cancellable = true, require = 0, target = {
 			@Desc(value = "rotate", ret = BlockState.class, args = {BlockState.class, Rotation.class}),
 			@Desc(value = "m_6843_", ret = BlockState.class, args = {BlockState.class, Rotation.class})
 			})
@@ -107,7 +107,7 @@ public abstract class MixinSlabBlock extends Block implements IVanillaSlabBlock
 	}
 
 
-	@Inject(at = @At("HEAD"), remap = false, cancellable = true, target = {
+	@Inject(at = @At("HEAD"), remap = false, cancellable = true, require = 0, target = {
 			@Desc(value = "mirror", ret = BlockState.class, args = {BlockState.class, Mirror.class}),
 			@Desc(value = "m_6943_", ret = BlockState.class, args = {BlockState.class, Mirror.class})
 	})
